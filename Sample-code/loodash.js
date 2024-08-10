d2f63e95320f3d139f93dc6dac03e55fd8494a4f
ReaganAdams <152213402+ReaganAdams@users.noreply.github.com>
2024-08-10 04:19:55 -0230

    loodash.js

---
+++ Sample-code/loodash.js
@@ -0,0 +1,11 @@
+// Import lodash library
+const _ = require('lodash');
+
+// Use lodash to work with an array of numbers
+const numbers = [1, 2, 3, 4, 5, 6];
+
+// Use lodash's shuffle method to shuffle the array
+const shuffledNumbers = _.shuffle(numbers);
+
+// Log the shuffled array to the console
+console.log('Shuffled Numbers:', shuffledNumbers);
